f4bdc261d0f8e17b97b57f42691b35614160641f
abhitruechamp <abhiaruvs3@gmail.com>
2024-06-26 02:53:25 +0530

    Added ability to add new books

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+
 const myLibrary = [new Book("Harry Potter", "JK Rowling", 250, true), new Book("Animal Farm", "George Orwell", 50, true)];
+showAllBooks();
 
 function Book(title, author, nPg, read){
     this.title = title;
@@ -16,26 +18,29 @@ function Book(title, author, nPg, read){
     //     console.log(`${this.title} by ${this.author}, ${this.nPg} pages, ${readStr}`)
     // }
 }
-function showBook(){
+function showAllBooks(){
     for(bk of myLibrary){
-        const tableRow = document.createElement("tr");
-        for(attribute in bk){
-            const tableData = document.createElement("td");
-            tableData.textContent = bk[attribute];
-            console.log(bk.attribute);
-            tableRow.appendChild(tableData);
-        }
-        let tableData = createTDwithButton("Read");
-        tableRow.appendChild(tableData);
-
-        tableData = createTDwithButton("Delete");
-        tableRow.appendChild(tableData);
-
-
-        
-        const table = document.querySelector('table');
-        table.appendChild(tableRow);
+        showBook(bk);
     }
+}
+
+function showBook(bk) {
+    const tableRow = document.createElement("tr");
+    for (attribute in bk) {
+        const tableData = document.createElement("td");
+        tableData.textContent = bk[attribute];
+        tableRow.appendChild(tableData);
+    }
+    let tableData = createTDwithButton("Read");
+    tableRow.appendChild(tableData);
+
+    tableData = createTDwithButton("Delete");
+    tableRow.appendChild(tableData);
+
+
+
+    const table = document.querySelector('table');
+    table.appendChild(tableRow);
 
     function createTDwithButton(text) {
         const tableData = document.createElement("td");
@@ -47,18 +52,41 @@ function showBook(){
     }
 }
 
-showBook();
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(bookDetailArray) {
+    [title, author, nPg, read] = bookDetailArray;
+    let bk = new Book(title, author, nPg, read)
+    myLibrary.push(bk)
   }
 
 
-  let btn = document.querySelector(".open-modal")
+  let openModal = document.querySelector(".open-modal")
   let dialog = document.querySelector("dialog")
   const closeBtn = document.querySelector(".close-modal")
-  btn.addEventListener("click", ()=>{
+  openModal.addEventListener("click", ()=>{
       dialog.showModal()
   })
+  let submitBtn = document.querySelector("button[type]")
+  submitBtn.addEventListener("click", (e)=>{
+
+    e.preventDefault();
+    const bookDetails = document.querySelectorAll(".book-form")
+    const bookDetailArray = [];
+    for(detail of bookDetails){
+       bookDetailArray.push(detail.value);
+    }
+    bookDetailArray[3] = bookDetailArray[3] == 'on'? true:false;
+    addAndShowBook(bookDetailArray); /*Won't work probably*/
+    dialog.close()
+  });
+
   closeBtn.addEventListener("click", ()=>{
       dialog.close()
-  })
+  });
+
+
+
+function addAndShowBook(bookDetailArray) {
+    addBookToLibrary(bookDetailArray);
+    showBook(myLibrary.slice(-1)[0]);
+}
+// See learnlinuxTV and fireship js course and TLE latest vid
